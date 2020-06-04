b3a286c39b93e8423e1ca365b2e4c762e1f5aa9e
programmer-heeney <programmer.heeney@gmail.com>
2020-06-04 14:00:01 +0100

    HTML: Remove static items + JS: Creat a single item dynamically, Statically Add information to HTML Elements

--- main.js
+++ main.js
@@ -1,4 +1,25 @@
 "use strict";
+// Create a Single item Dynamically Using JS
+const ul = document.createElement("ul");
+const li = document.createElement("li");
+const img = document.createElement("img");
+const p = document.createElement("p");
+const itemsContainer = document.querySelector(".items__container");
+
+itemsContainer.append(ul);
+li.append(img);
+li.append(p);
+ul.append(li);
+
+p.innerText = "male, small size";
+ul.setAttribute("calss", "items");
+li.setAttribute("class", "item");
+li.setAttribute("data-color", "blue");
+li.setAttribute("data-type", "p");
+img.setAttribute("class", "item__icon");
+img.setAttribute("src", "imgs/blue_p.png");
+p.setAttribute("class", "item__name");
+
 // Handle click on buttons on category
 const categories = document.querySelectorAll(".categories");
 const items = document.querySelectorAll(".item");
